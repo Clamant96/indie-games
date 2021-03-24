9f29634e2ce9163a3bbe8a628d73d0282d1b2d62
Clamant96 <kevin.lazzarotto@hotmail.com>
2021-03-24 10:47:10 -0300

    Cricao de model, repository e controller de jogo

---
+++ indieGames/indieGames/src/main/java/com/exploregames/indieGames/model/Jogo.java
@@ -0,0 +1,66 @@
+package com.exploregames.indieGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "jogo")
+public class Jogo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(max = 50)
+	private String nome;
+	
+	@NotNull
+	@Size(max = 1000)
+	private String descricao;
+	
+	@NotNull
+	private double preco;
+	
+	/* criar a desenvolvedora
+	 * criar a plataforma
+	 */
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+}
